9362e2a4bb189c15b992d13172504d0283bd63dd
abelofficial <abelofficial94@gmail.com>
2021-10-09 14:43:10 +0200

    Added basic animation on tech stack section.

--- src/utils/globalTheme.js
+++ src/utils/globalTheme.js
@@ -113,6 +113,7 @@ const theme = (darkMode = false) =>
 
     shadows: [
       'none',
+      '0px 2px 1px -1px rgba(255,196,12,0.2),0px 1px 1px 0px rgba(255,196,12,0.14),0px 1px 3px 0px rgba(255,196,12,0.12)',
       '0px 3px 5px -1px rgba(255,196,12,0.2),0px 5px 8px 0px rgba(255,196,12,0.14),0px 1px 14px 0px rgba(255,196,12,0.12)'
     ]
     // shadows: {},
