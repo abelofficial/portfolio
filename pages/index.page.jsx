d0fbca82ba66975e3e5ff273bde79ea606702b73
Abel Sintaro <abelofficial94@gmail.com>
2022-02-09 21:37:02 +0100

    Updated TechStack component to MU-5.

--- pages/index.page.jsx
+++ pages/index.page.jsx
@@ -2,12 +2,14 @@
 
 // Material
 import { Box, Typography } from "@mui/material";
+import { motion } from "framer-motion";
+import { NextSeo } from "next-seo";
 
 // Local
-// import TechStack from "@local/src/components/sections/TechStack";
+import TechStack from "@local/src/components/sections/TechStack";
 // import Specialty from "@local/src/components/sections/Specialty";
 // import GitHubCalendar from "@local/src/components/sections/GithubActivities";
-// import routeAnim from "@local/src/assets/routeAnimation.json";
+import routeAnim from "@local/src/assets/routeAnimation.json";
 
 // Style
 import useStyle from "./index.style";
@@ -18,20 +20,22 @@ export default function Home() {
   return (
     <Box>
       <Typography> Home Page</Typography>
-      {/* <NextSeo title='Abel Sintaro - Home' />
+      <NextSeo title='Abel Sintaro - Home' />
       <motion.div exit={{ opacity: 0 }} initial='initial' animate='animate'>
         <motion.div variants={routeAnim.stagger}>
           <motion.div variants={routeAnim.fadeInUp}>
             <TechStack />
           </motion.div>
           <motion.div variants={routeAnim.fadeInUp}>
-            <GitHubCalendar />
+            {/* <GitHubCalendar /> */}
+            <h1>Section</h1>
           </motion.div>
           <motion.div id='landing-services' variants={routeAnim.fadeInUp}>
-            <Specialty />
+            {/* <Specialty /> */}
+            <h1>Section</h1>
           </motion.div>
         </motion.div>
-      </motion.div> */}
+      </motion.div>
     </Box>
   );
 }
