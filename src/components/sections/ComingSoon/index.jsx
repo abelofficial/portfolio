69728277128ed56cffa43d5e6f4ce1bf3f9b2138
Abel Sintaro <abelofficial94@gmail.com>
2022-02-21 19:40:06 +0100

    Added coming soon anim to unimplemented pages.

---
+++ src/components/sections/ComingSoon/index.jsx
@@ -0,0 +1,34 @@
+// Core
+import Lottie from "react-lottie-player";
+
+// Material ui
+import { useTheme } from "@mui/styles";
+// Local
+import { SectionContainer } from "@local-components/hoc";
+import * as animationPlane from "public/animations/coming_soon.json";
+import * as animationPlaneDarkTheme from "public/animations/coming_soon_dark.json";
+
+// Component style
+import useStyles from "./style";
+
+const ComingSoon = (props) => {
+  const theme = useTheme();
+  const styles = useStyles()();
+
+  return (
+    <SectionContainer>
+      <Lottie
+        loop
+        play
+        animationData={
+          theme.palette.type === "light"
+            ? animationPlane
+            : animationPlaneDarkTheme
+        }
+        className={styles.anim}
+      />
+    </SectionContainer>
+  );
+};
+
+export default ComingSoon;
