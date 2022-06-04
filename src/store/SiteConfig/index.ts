9881a254a388d9baeb6a7358eb4c754085e9844b
Abel Sintaro <abelofficial94@gmail.com>
2022-06-04 12:36:44 +0200

    Setup redux.

---
+++ src/store/SiteConfig/index.ts
@@ -0,0 +1,74 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+// Define a type for the slice state
+interface ConfigState {
+  drawer: boolean;
+  showBurgerMenu: boolean;
+  darkMode: boolean;
+  sideBarPage: {
+    summary: boolean;
+    lock: boolean;
+  };
+}
+
+// Define the initial state using that type
+const initialState: ConfigState = {
+  drawer: false,
+  showBurgerMenu: false,
+  darkMode: false,
+  sideBarPage: {
+    summary: false,
+    lock: false,
+  },
+};
+
+export const configsSlice = createSlice({
+  name: "configs",
+  initialState,
+  reducers: {
+    // Action to toggle between dark and light mode
+    openDrawer: (state) => {
+      state.drawer = true;
+    },
+    closeDrawer: (state) => {
+      state.drawer = false;
+    },
+    setDarkMode: (state) => {
+      state.darkMode = true;
+    },
+    turnOffDarkMode: (state) => {
+      state.darkMode = false;
+    },
+    setSideBarSummary: (state, action) => {
+      if (!state.sideBarPage.lock) state.sideBarPage.summary = action.payload;
+    },
+    setSideBarSummaryLock: (state, action: PayloadAction<boolean>) => {
+      state.sideBarPage.lock = action.payload;
+    },
+    showBurgerMenu: (state) => {
+      state.showBurgerMenu = true;
+    },
+    hideBurgerMenu: (state) => {
+      state.showBurgerMenu = false;
+    },
+  },
+});
+
+export const {
+  openDrawer,
+  closeDrawer,
+  setDarkMode,
+  turnOffDarkMode,
+  setSideBarSummary,
+  setSideBarSummaryLock,
+  showBurgerMenu,
+  hideBurgerMenu,
+} = configsSlice.actions;
+
+// Selectors
+export const selectDrawer = (state) => state.siteConfig.drawer;
+export const selectDarkMode = (state) => state.siteConfig.darkMode;
+export const selectSideBarPage = (state) => state.siteConfig.sideBarPage;
+export const selectBurgerMenu = (state) => state.siteConfig.showBurgerMenu;
+
+export default configsSlice.reducer;
