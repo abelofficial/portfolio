60d9b37a7809effa0ded2e0e912891ca08a8596c
Abel Sintaro <abelofficial94@gmail.com>
2022-06-17 16:06:49 +0200

    Updated tech stack component design.

--- src/components/TechStack/style.ts
+++ src/components/TechStack/style.ts
@@ -1,52 +1,34 @@
 import { createStyles } from "@mantine/core";
 
 const styles = createStyles((theme) => ({
-  "@keyframes slideLeft": {
-    from: {
-      transform: "translateX(-10%)",
-    },
-    to: {
-      transform: "translateX(10%)",
-    },
-  },
-  "@keyframes slideRight": {
-    from: {
-      transform: "translateX(10%)",
-    },
-    to: {
-      transform: "translateX(-10%)",
-    },
-  },
-  container: {
-    padding: theme.spacing.xs,
-  },
   section: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
     padding: theme.spacing.xs,
-  },
-  divider: {
-    marginTop: theme.spacing.sm,
-    marginBottom: theme.spacing.sm,
-    opacity: 0.4,
-    filter: "blur(0.1rem)",
-  },
-  slideAnim: {
-    flexWrap: "wrap",
-    overflow: "hidden",
-    borderRadius: theme.defaultRadius,
+    wrap: "no-wrap",
+    overflow: "scroll",
+    scrollbarWidth: "none",
+    paddingTop: theme.spacing.lg,
+    "-ms-overflow-style": "none",
+
+    "::-webkit-scrollbar": {
+      display: "none",
+    },
   },
 
+  text: {
+    color: theme.primaryColor[4],
+    paddingTop: theme.spacing.xs,
+  },
   frameworkContainer: {
-    // marginRight: theme.spacing(2),
-    position: "relative",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-  },
-  animWaveLeft: {
-    animation: `$slideLeft 1500ms ease 100ms infinite alternate`,
-  },
-  animWaveRight: {
-    animation: `$slideRight 1500ms ease 100ms infinite alternate`,
+    minWidth: "25%",
+    [`@media (min-width: ${theme.breakpoints.md}px)`]: {
+      minWidth: "10%",
+    },
   },
 }));
 
