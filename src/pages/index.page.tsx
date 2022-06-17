c1f174bd2318b6bd55f165619601b339237f97d0
Abel Sintaro <abelofficial94@gmail.com>
2022-06-17 17:16:21 +0200

    Moved specialty to timeline page.

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { motion } from "framer-motion";
 import routeAnim from "@utils/routeAnimation.json";
 import TechStack from "@components/TechStack";
-import Specialty from "@components/Specialty";
 import GithubSummery from "@components/GithubSummery";
 
 const Index = () => {
@@ -13,9 +12,6 @@ const Index = () => {
       </motion.div>
       <motion.div variants={routeAnim.fadeInUp}>
         <TechStack />
-      </motion.div>
-      <motion.div variants={routeAnim.fadeInUp}>
-        <Specialty />
       </motion.div>
     </motion.div>
   );
