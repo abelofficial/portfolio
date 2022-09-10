fdbd0a7984fd2fa6901219a5affde9d49bd16d9d
Abel Sintaro <abelofficial94@gmail.com>
2022-09-10 17:57:38 +0200

    Updated Specialty section layout.

--- src/components/Specialty/index.tsx
+++ src/components/Specialty/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { v4 as uuid } from "uuid";
 import Image from "next/image";
-import { Box, Container, Divider, Grid, Title } from "@mantine/core";
+import { Box, Grid, Title } from "@mantine/core";
 import data from "@static/specialtyData.json";
 import useStyles from "./style";
 import InfoContainer from "@hoc/InfoContainer";
@@ -15,21 +15,30 @@ const Index = () => {
         <Grid.Col span={1}>
           <Title order={4}>Specialty</Title>
         </Grid.Col>
-        {data.map((item, _i) => (
-          <Box component={Grid.Col} span={1} key={uuid()}>
-            <Divider className={classes.divider} />
-            <Container className={classes.section}>
-              <Image
-                alt={item.title + " Image"}
-                src={item.icon}
-                width={60}
-                height={60}
-              />
-              <Title order={5}>{item.title}</Title>
-              <p>{item.content}</p>
-            </Container>
-          </Box>
-        ))}
+        <Grid.Col className={classes.listContainer}>
+          <Grid>
+            {data.map((item, _i) => (
+              <Box
+                component={Grid.Col}
+                span={11}
+                xs={11}
+                sm={10}
+                md={5}
+                key={uuid()}
+                className={classes.card}
+              >
+                <Image
+                  alt={item.title + " Image"}
+                  src={item.icon}
+                  width={60}
+                  height={60}
+                />
+                <Title order={5}>{item.title}</Title>
+                <p className={classes.text}>{item.content}</p>
+              </Box>
+            ))}
+          </Grid>
+        </Grid.Col>
       </Grid>
     </InfoContainer>
   );
--- src/components/Specialty/style.ts
+++ src/components/Specialty/style.ts
@@ -20,14 +20,39 @@ const styles = createStyles((theme) => ({
   container: {
     padding: theme.spacing.xs,
   },
-  section: {
-    padding: theme.spacing.xs,
-  },
+  section: {},
   divider: {
     marginTop: theme.spacing.sm,
     marginBottom: theme.spacing.sm,
     opacity: 0.4,
     filter: "blur(0.1rem)",
+  },
+  card: {
+    margin: `1rem`,
+    borderRadius: theme.radius.md,
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[7]
+        : theme.colors.gray[0],
+    maxWidth: "100%",
+    padding: theme.spacing.xs,
+  },
+  listContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+
+    [`@media (min-width: ${theme.breakpoints.sm}px)`]: {
+      flexDirection: "row",
+    },
+  },
+  text: {
+    margin: "0 0 1rem",
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.gray[1]
+        : theme.colors.gray[4],
   },
 }));
 
