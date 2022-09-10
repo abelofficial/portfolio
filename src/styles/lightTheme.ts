fa73a17fc15a7e799088b2a59da692ad5285150d
Abel Sintaro <abelofficial94@gmail.com>
2022-09-10 15:27:46 +0200

    Removed box shadow from the InfoContainer component. (#26)

--- src/styles/lightTheme.ts
+++ src/styles/lightTheme.ts
@@ -6,12 +6,21 @@ const theme: MantineThemeOverride = {
   colorScheme: "light",
   primaryColor: ["#ffc40c", "#f9ff0c", "#ffe40c", "#ccb609", "#ccb409"],
   shadows: {
-    xs: `0px 2px 1px -1px rgba(${shadowColor},0.2),0px 1px 1px 0px rgba(${shadowColor},0.14),0px 1px 3px 0px rgba(${shadowColor},0.12)`,
+    xs: `0px 3px 3px -2px rgba(${shadowColor},0.2),0px 3px 4px 0px rgba(${shadowColor},0.14),0px 1px 8px 0px rgba(${shadowColor},0.12)`,
     sm: `0px 3px 3px -2px rgba(${shadowColor},0.2),0px 3px 4px 0px rgba(${shadowColor},0.14),0px 1px 8px 0px rgba(${shadowColor},0.12)`,
   },
   colors: {
     backgroundPrimary: ["#FFF", ""],
     backgroundSecondary: ["#FFF", "#F2F2F2"],
+    gray: [
+      "#eee",
+      "#BDBDBD",
+      "#9E9E9E",
+      "#757575",
+      "#616161",
+      "#424242",
+      "#212121",
+    ],
   },
 };
 
